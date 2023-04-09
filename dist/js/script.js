24f21c8d348db7367620a50f7abb2a0a712e9710
Jamal <jamal.rzayev@gmail.com>
2023-04-10 00:19:55 +0400

    advantages_screen_desktop_done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
     $('.main__carousel_inner').slick({
       speed: 1200,
-      // adaptiveHeight: true,
+      adaptiveHeight: true,
       autoplay: true,
       dots: true,
       appendDots:$('.dots'),
       prevArrow: $('.main__carousel_prev'),
       nextArrow: $('.main__carousel_next'),
-      // prevArrow: '<button type="button" class="main__carousel_prev"><img src="icons/main/rounded_rectangle_left.svg"></button>',
-      // nextArrow: '<button type="button" class="main__carousel_next"><img src="icons/main/rounded_rectangle_right.svg"></button>',
       responsive: [
         {
             breakpoint: 768,
