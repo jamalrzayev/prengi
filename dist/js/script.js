7a27c052a0d372e2e29888e4ce4b5298abf1d44b
Jamal <jamal.rzayev@gmail.com>
2023-04-16 20:50:12 +0400

    products_section done

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,14 +7,22 @@ $(document).ready(function(){
       appendDots:$('.dots'),
       prevArrow: $('.main__carousel_prev'),
       nextArrow: $('.main__carousel_next'),
-      responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-              dots: true,
-              arrows: false
-            }
-        }
-      ]
+      // responsive: [
+      //   {
+      //       breakpoint: 768,
+      //       settings: {
+      //         dots: true,
+      //         arrows: false
+      //       }
+      //   }
+      // ]
+    });
+    $('.products__slider_content').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      autoplay: true,
+      dots: false,
+      prevArrow: $('.products__slider-prev'),
+      nextArrow: $('.products__slider-next'),
     });
   });
